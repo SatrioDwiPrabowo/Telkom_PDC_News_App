ed5ffa4e3ee8ee82f4b96fba972c11f1dd755ac7
SatrioDwiPrabowo <sdprabowo77@gmail.com>
2016-03-26 02:43:46 +0700

    'Telkom PDC Karawang News App'

---
+++ app/src/main/java/dev/telkompdckarawang/telkomnews/PDCMainActivity.java
@@ -0,0 +1,16 @@
+package dev.telkompdckarawang.telkomnews;
+
+//First Coding Added AppCompactActivity
+//SatrioDwiPrabowo
+//Asep Eka
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PDCMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdcmain);
+    }
+}
