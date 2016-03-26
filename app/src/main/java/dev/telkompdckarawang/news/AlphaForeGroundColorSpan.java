488840dc01280deb9c4a61e88f3693c5b8dece11
Satrio Dwi Prabowo <sdprabowo77@gmail.com>
2016-03-26 13:47:29 +0700

    'Added New Java File'

---
+++ app/src/main/java/dev/telkompdckarawang/news/AlphaForeGroundColorSpan.java
@@ -0,0 +1,35 @@
+package dev.telkompdckarawang.news;
+//author 	: SatrioDwiPrabowo
+//designer 	: Asep Eka M
+//Company 	: CCDP Telkom PDC Karawang
+import android.graphics.Color;
+import android.text.TextPaint;
+import android.text.style.ForegroundColorSpan;
+
+
+public class AlphaForeGroundColorSpan extends ForegroundColorSpan {
+
+    private float mAlpha;
+
+    public AlphaForeGroundColorSpan(int color) {
+        super(color);
+    }
+
+    @Override
+    public void updateDrawState(TextPaint ds) {
+        ds.setColor(getAlphaColor());
+    }
+
+    public void setAlpha(float alpha) {
+        mAlpha = alpha;
+    }
+
+    public float getAlpha() {
+        return mAlpha;
+    }
+
+    private int getAlphaColor() {
+        int foregroundColor = getForegroundColor();
+        return Color.argb((int) (mAlpha * 255), Color.red(foregroundColor), Color.green(foregroundColor), Color.blue(foregroundColor));
+    }
+}
